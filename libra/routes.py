40e761bd4f3bfb9a3a933ca6aef12f76af275a43
Mohit Hulse <hulsemohit@gmail.com>
2022-01-11 14:52:54 +0530

    Improved home and about pages. Other minor changes.

--- libra/routes.py
+++ libra/routes.py
@@ -29,6 +29,11 @@ def home():
     return render_template("home.html")
 
 
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
 @app.route("/register", methods=["GET", "POST"])
 def register():
     if current_user.is_authenticated:
